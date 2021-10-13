80494769148ed089a9aa97826cf9a599b88236ac
DELL <DELL@DESKTOP-OGRVV23>
2021-10-13 14:23:55 +0700

    baitapham

---
+++ src/baitapham/Baitapham.java
@@ -0,0 +1,17 @@
+
+package baitapham;
+
+
+public class Baitapham {
+
+
+    public static void show() {
+         System.out.println("Lập trình java 2021");
+    }
+    public static void main(String[] args) {
+		show();
+}
+}
+    
+    
+
